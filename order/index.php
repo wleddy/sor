a89b9494c5f2861f6029232990afc341a363b82e
Bill Leddy <bill@williesworkshop.net>
2016-01-26 12:04:58 -0800

    fix css for order page

--- order/index.php
+++ order/index.php
@@ -3,7 +3,17 @@
 $pageTitle = 'Order Tickets';
 
 ## Add items to the header if needed
-#$extraCSS = '<link rel="stylesheet" href="/css/emf.css" type="text/css" media="all" >';
+$extraCSS = '
+    <!-- emailmeform Styles -->
+    <link rel="stylesheet" type="text/css" href="http://www.emailmeform.com/builder/theme_css/Rdsd6MSo8p0w7g">
+    <!-- emailmeform header stuff -->
+    <link rel="stylesheet" type="text/css" href="//assets.emailmeform.com/styles/dynamic.php?t=post&amp;enable_responsive_ui=1&amp;RU1GLTAyLTE3LTk5NTU%3D">
+    <style type="text/css">
+        html{background-color:black;}
+        body{margin:36px auto;}
+    </style>
+
+';
 #$extraHeaders = '';
 #$extraJS = '';
 
