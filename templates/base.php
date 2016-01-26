f9a9a3a96e3fb093c8e82693f4b3acb66a12282b
Bill Leddy <bill@williesworkshop.net>
2016-01-25 20:31:45 -0800

    reconfigure pages to use update base.php

--- templates/base.php
+++ templates/base.php
@@ -1,8 +1,6 @@
 <?php
-
-# This file passes the content of the Readme.md file in the same directory
-# through the Markdown filter. You can adapt this sample code in any way
-# you like.
+$SiteName = "";
+if ($SiteName == "") $SiteName = $_SERVER['SERVER_NAME'];
 
 # Install PSR-0-compatible class autoloader
 spl_autoload_register(function($class){
@@ -12,16 +10,40 @@ spl_autoload_register(function($class){
 # Get Markdown class
 use \Michelf\Markdown;
 
-## setup some placeholder vars
-$extraCSS = '';
-$extraHeaders = '';
-$extraJS = '';
+function prepPath($path){
+    # ensure that path has a leading "/" and no trailing "/"
+    if(substr($path,0,1) != "/"){
+        $path = "/" + $path;
+    }
+    if (substr($path,-1) == "/"){
+        $path = substr($path,0,-1);
+    }
+    return trim($path);
+}
+
+/* 
+    'content.xx' is the default name for the main page body but having all the pages named
+    "content" is confusing during editing. 
+    This change allows you to name the main content file after the enclosing directory name
+*/
+$requestURI = prepPath($_SERVER['REQUEST_URI']);
+$docRoot = prepPath($_SERVER['DOCUMENT_ROOT']);
+
+#ensure that there is a trailing '/' in $requestURI
+#if (substr($requestURI,strlen($requestURI)-1) != '/' ) {$requestURI = $requestURI . '/';}
+$bits = explode("/", $requestURI . "/");
+$localDir = $bits[count($bits)-2];
+
+if ($localDir=="") {$localDir = "arandomstring";} ## not sure how the file system will handle an empty file name
 
 ## Get the page content -- adding elements here will add divs to html scaffold
+## Note that ONLY the $localDir element has the 'id' property
+## If there is a file that matches the directory name, the 'content' file will not be used
 $htmlContainers = array("head" => array('text' => '','filepath' => ''),
 						"nav" => array('text' => '','filepath' => ''),
 						"left" => array('text' => '','filepath' => ''),
-						"content" => array('text' => '<h1>No content for the '.$pageTitle.' page yet...</h1>','filepath' => ''),
+						$localDir => array('id' => 'content', 'text' => '','filepath' => ''),
+						"content" => array('text' => '','filepath' => ''),
 						"foot" => array('text' => '','filepath' => '')
 						);
 
@@ -29,7 +51,7 @@ foreach($htmlContainers as $baseName => $x_value){
 	#look first in local dir for .php, then .html, then .md
 	#if not local, look in templates dir. 
 	#Only ever use one file at most for each section
-	if (file_exists($baseName.'.php')) {
+	 if (file_exists($baseName.'.php')) {
 		$htmlContainers[$baseName]['filepath'] = $baseName.'.php';
 	}elseif (file_exists($baseName.'.html')) {
 			$htmlContainers[$baseName]['text'] = file_get_contents($baseName.'.html');
@@ -39,43 +61,66 @@ foreach($htmlContainers as $baseName => $x_value){
 		$htmlContainers[$baseName]['text'] = Markdown::defaultTransform($text);
 	}
 		#check the default files
-	elseif(file_exists($_SERVER['DOCUMENT_ROOT']."/templates/".$baseName.'.php')){
-		$htmlContainers[$baseName]['filepath'] = $_SERVER['DOCUMENT_ROOT']."/templates/".$baseName.'.php';
-	}elseif(file_exists($_SERVER['DOCUMENT_ROOT']."/templates/".$baseName.'.html')){
-		$htmlContainers[$baseName]['text'] = file_get_contents($_SERVER['DOCUMENT_ROOT']."/templates/".$baseName.'.html');
-	}elseif (file_exists($_SERVER['DOCUMENT_ROOT']."/templates/".$baseName.'.md')) {
+	elseif(file_exists($docRoot."/templates/".$baseName.'.php')){
+		$htmlContainers[$baseName]['filepath'] = $docRoot."/templates/".$baseName.'.php';
+	}elseif(file_exists($docRoot."/templates/".$baseName.'.html')){
+		$htmlContainers[$baseName]['text'] = file_get_contents($docRoot."/templates/".$baseName.'.html');
+	}elseif (file_exists($docRoot."/templates/".$baseName.'.md')) {
 		# Read file and pass content through the Markdown parser
-		$text = file_get_contents($_SERVER['DOCUMENT_ROOT']."/templates/".$baseName.'.md');
+		$text = file_get_contents($docRoot."/templates/".$baseName.'.md');
 		$htmlContainers[$baseName]['text'] = Markdown::defaultTransform($text);
 	}
-}
+} #for htmlConainers
 
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php echo($pageTitle);?> : Safari on the River</title>
+        <title><?php echo($pageTitle);?> - <?php echo($SiteName)?></title>
 		<link rel="icon" type="image/png" href="/images/favoricon.png" >
 	    <link rel="SHORTCUT ICON" type="image/vnd.microsoft.icon" href="/images/favricon.ico" >
+		<?php echo($extraHeaders);?>
+
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 		<script type="text/javascript" src="/js/common.js" ></script>
-		<script type="text/javascript" >
-			$j(document).ready(function(){doReady();});
-		</script>
-		
+		<?php
+		    $js = $requestURI . "/" . $localDir . '.js';
+		    if(file_exists($docRoot.$js)){
+		    echo('<script type="text/javascript" src="'.$js.'" ></script>');
+		    }
+		?>
 		<?php echo($extraJS);?>
 		
-		<link rel="stylesheet" href="/css/styles.css" type="text/css" media="all" >
-	    
-		<?php echo($extraCSS);?>
-		<?php echo($extraHeaders);?>
+		<link rel="stylesheet" href="/css/default.css" type="text/css" media="all" >
+		<?php
+		    $css = $requestURI . "/" . $localDir . '.css';
+		    if(file_exists($docRoot. $css)){
+		    ?>
+		    <link rel="stylesheet" href="<?php echo($css);?>" type="text/css" media="all" >
+		    <?php
+		    }
+		echo($extraCSS);?>
+		
     </head>
     <body>
+    <div id="container" >
 <?php
+    $useLocalDir = false;
 	foreach($htmlContainers as $baseName => $x_value){
 		if(($htmlContainers[$baseName]['filepath'] != '') || ($htmlContainers[$baseName]['text'] != '')) {
-			echo '<div id="'.$baseName.'">'."\r\n";
+		    $divID = $baseName;
+		    ## only the $localFile element contains the 'id' element
+		    if ($htmlContainers[$baseName]['id']) {
+        	    $divID = $htmlContainers[$baseName]['id'];
+        	    $useLocalDir = true;
+        	}
+        	if ($useLocalDir && ($baseName == 'content')) {
+        	    ## if we used the local file, we don't use 'content'
+        	    continue;
+        	}
+
+			echo '<div id="'.$divID.'">'."\r\n";
 			if($htmlContainers[$baseName]['filepath'] != ''){
 				require $htmlContainers[$baseName]['filepath'];
 			}else{
@@ -85,5 +130,6 @@ foreach($htmlContainers as $baseName => $x_value){
 		} 
 	}
 ?>
+    </div>
     </body>
 </html>
