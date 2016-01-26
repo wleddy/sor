2398b518b7943a83142fc6f52b7bc8297da27564
Bill Leddy <bill@williesworkshop.net>
2016-01-26 12:30:00 -0800

    more style changes for the order page

--- order/index.php
+++ order/index.php
@@ -9,8 +9,15 @@ $extraCSS = '
     <!-- emailmeform header stuff -->
     <link rel="stylesheet" type="text/css" href="//assets.emailmeform.com/styles/dynamic.php?t=post&amp;enable_responsive_ui=1&amp;RU1GLTAyLTE3LTk5NTU%3D">
     <style type="text/css">
+        /* beat email-for-me into submission  */
         html{background-color:black;}
         body{margin:36px auto;}
+        .emf-section-text, #emf-container, #emf-container ul li, 
+        #emf-container .emf-clear, .efm-clear, #emf-container ul li .emf-section-text
+        {
+            clear:none;
+        }
+        
     </style>
 
 ';
